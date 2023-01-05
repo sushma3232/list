a84339cf56152c58e488b2a10815239df162f410
sushma3232 <121096745+sushma3232@users.noreply.github.com>
2023-01-05 14:32:56 +0530

    Add files via upload

---
+++ 1.fuc.ano.fun.py
@@ -0,0 +1,71 @@
+# def greatest(a,b):
+#     if a>b:
+#         return a
+#     else:
+#         return b
+# def greatest(a,b,c):
+#     if greater a,b>c:
+#         return greater(a,b)
+#     else:
+#         return c
+    
+# greatest(12,10,9)
+
+
+
+
+# def a(a,b):
+#     c=a+b
+#     return c
+# def t():
+#     return a(3,4)
+# print(t())
+
+
+
+
+# def sum(a,b):
+#     if a>b:
+#         return a
+#     else:
+#         return b
+# def num(g):
+#     if g>0:
+#         return g
+#     else:
+#         return g
+#     print(sum(5,6),"is max")
+# z=num(8)
+# print(z)
+
+
+
+# def sum(a,b):
+#     c=a+b
+#     print(c)
+# print(sum(7,8))
+
+# a=4,5
+# print(a)
+
+
+
+def sum(*a):
+    num=0
+    z=list(a)
+    i=0
+    while i<len(z):
+        num+=z[i]
+        i=i+1
+    print(num)
+sum(8,2,3,0,7)
+
+
+
+# def div(a,b):
+#     if b!=0:
+#         return a/b
+#     else:
+#         return 0
+    
+
